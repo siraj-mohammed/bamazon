83804e6a138a596aa5a6d4c9f3b7b4c06f97ead9
MaximusSirilius <40532864+siraj-mohammed@users.noreply.github.com>
2018-10-01 01:31:45 -0400

    Added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// Import required libraries
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Create a connection to MySQL
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306, // The port number your MySQL is listening on
@@ -9,14 +11,35 @@ var connection = mysql.createConnection({
     database: "bamazon"
   });
 
-  connection.connect(function(err) {
+// Open the connection to MySQL
+connection.connect(function(err) {
     if (err) throw err;
     if (connection.threadId){
+        // When connection to MySQL is successful, run a SQL query and list all items in inventory
         listProducts();
     }
 });
 
 console.log("Welcome to Bamazon!");
+
+// Run a Select all query against the database and prettify the results
+function listProducts(){
+    connection.query(
+        "SELECT * FROM products",
+        function(err, results){
+            if (err) throw err;
+            for (var i = 0; i < results.length; i++){
+                console.log(
+                    "Item Id: " + results[i].item_id +
+                    " || Product Name: " + results[i].product_name +
+                    " || Department: " + results[i].department_name +
+                    " || Price($): " + results[i].price
+                );
+            }
+            console.log("---------------------------------------------");
+            promptUser(); // Once inventory is displayed, invoke the inquirer prompt
+        });
+}
 
 function promptUser(){
     inquirer.prompt([
@@ -49,16 +72,16 @@ function promptUser(){
         connection.query(
             "SELECT * FROM products WHERE ?",
             {
-                item_id: answer.item_id
+                item_id: answer.item_id // Select only the item that has an ID matching user's input
             },
             function(err, results){
                 if (err) throw err;
-                if(results.length > 0){
-                    var product = results[0];
-                    if (answer.quantity <= product.stock_quantity){
+                if(results.length > 0){ // If the user selected a valid ID and a response was received from the database
+                    var product = results[0]; // Since IDs are unique per product, select the first item in the result array 
+                    if (answer.quantity <= product.stock_quantity){ // If there's enough in stock to process user's order
                         console.log("Processing your order...");
                         connection.query(
-                            "UPDATE products SET ? WHERE ?",
+                            "UPDATE products SET ? WHERE ?", // Update stock inventory
                             [
                               {
                                 stock_quantity: product.stock_quantity - answer.quantity
@@ -69,6 +92,7 @@ function promptUser(){
                             ],
                             function(err) {
                               if (err) throw err;
+                              // Display confirmation and total $ amount of purchase to user
                               console.log("Your order has been processed");
                               console.log("Total cost of your purchase: $" + product.price * answer.quantity);
                               console.log("Thank you for shopping at Bamazon!");
@@ -76,35 +100,17 @@ function promptUser(){
                               connection.end();
                             });
                     }
-                    else {
-                        console.log("Insufficient quantity in stock. Unable to process your order at this time");
+                    else { // If the requested quantity is not available..
+                        console.log("Insufficient quantity in stock. Sorry, we are unable to process your order at this time");
                         console.log("Please enter a lower quantity or select another product. Thank you!");
                         console.log("---------------------------------------------");
-                        listProducts();
+                        listProducts(); // Show inventory
                     }
-                } else {
+                } else { // If the user entered an invalid number for ID, and no response was received from MySQL
                     console.log("ERROR. Please enter a valid Item ID");
-                    listProducts();
+                    listProducts(); // Show inventory
                 }
             }
         )
     });
-}
-
-function listProducts(){
-    connection.query(
-        "SELECT * FROM products",
-        function(err, results){
-            if (err) throw err;
-            for (var i = 0; i < results.length; i++){
-                console.log(
-                    "Item Id: " + results[i].item_id +
-                    " || Product Name: " + results[i].product_name +
-                    " || Department: " + results[i].department_name +
-                    " || Price($): " + results[i].price
-                );
-            }
-            console.log("---------------------------------------------");
-            promptUser();
-        });
 }
